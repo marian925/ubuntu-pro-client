1f2cb68ce7300bb299d268d9d16971b73fc7045b
Lucas Moura <lucas.moura@canonical.com>
2024-02-26 14:42:39 -0300

    cfg: remove status cache from UAConfig
    The UAConfig class should not handle the status cache as it is not
    tied to any Pro Client configuration. We have created a separate
    file object for it that is being handled directly by the status
    module now

--- uaclient/files/machine_token.py
+++ uaclient/files/machine_token.py
@@ -1,104 +1,11 @@
 import json
-import logging
 import os
 from datetime import datetime
 from typing import Any, Dict, Optional  # noqa: F401
 
-from uaclient import defaults, event_logger, exceptions, system, util
+from uaclient import defaults, exceptions, system, util
 from uaclient.contract_data_types import PublicMachineTokenData
-
-event = event_logger.get_event_logger()
-LOG = logging.getLogger(util.replace_top_level_logger_name(__name__))
-
-
-class UAFile:
-    def __init__(
-        self,
-        name: str,
-        directory: str = defaults.DEFAULT_DATA_DIR,
-        private: bool = True,
-    ):
-        self._directory = directory
-        self._file_name = name
-        self._is_private = private
-        self._path = os.path.join(self._directory, self._file_name)
-
-    @property
-    def path(self) -> str:
-        return self._path
-
-    @property
-    def is_private(self) -> bool:
-        return self._is_private
-
-    @property
-    def is_present(self):
-        return os.path.exists(self.path)
-
-    def write(self, content: str):
-        file_mode = (
-            defaults.ROOT_READABLE_MODE
-            if self.is_private
-            else defaults.WORLD_READABLE_MODE
-        )
-        # try/except-ing here avoids race conditions the best
-        try:
-            if os.path.basename(self._directory) == defaults.PRIVATE_SUBDIR:
-                os.makedirs(self._directory, mode=0o700)
-            else:
-                os.makedirs(self._directory)
-        except OSError:
-            pass
-
-        system.write_file(self.path, content, file_mode)
-
-    def read(self) -> Optional[str]:
-        content = None
-        try:
-            content = system.load_file(self.path)
-        except FileNotFoundError:
-            LOG.debug("Tried to load %s but file does not exist", self.path)
-        return content
-
-    def delete(self):
-        system.ensure_file_absent(self.path)
-
-
-class ProJSONFile:
-    def __init__(
-        self,
-        name: str,
-        directory: str = defaults.DEFAULT_DATA_DIR,
-        private: bool = True,
-    ):
-        self._file = UAFile(name=name, directory=directory, private=private)
-
-    def write(self, content: Dict[str, Any]):
-        self._file.write(
-            content=json.dumps(content, cls=util.DatetimeAwareJSONEncoder)
-        )
-
-    def read(self) -> Optional[Dict[str, Any]]:
-        content = self._file.read()
-
-        if content:
-            return json.loads(content, cls=util.DatetimeAwareJSONDecoder)
-
-        return None
-
-    def delete(self):
-        return self._file.delete()
-
-    @property
-    def is_present(self):
-        return self._file.is_present
-
-
-class UserCacheFile(UAFile):
-    def __init__(self, name: str):
-        super().__init__(
-            name, directory=system.get_user_cache_dir(), private=False
-        )
+from uaclient.files.files import ProJSONFile
 
 
 class MachineTokenFile:
